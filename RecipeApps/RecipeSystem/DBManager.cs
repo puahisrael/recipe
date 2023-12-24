cd10dbe059a9f9430edde8d9073f3b1721fe5f78
puahisrael <puahisrael1@gmail.com>
2023-12-24 13:09:45 -0600

    work

--- RecipeApps/RecipeSystem/DBManager.cs
+++ RecipeApps/RecipeSystem/DBManager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RecipeSystem
 {
-    internal class DBManager
+    public class DBManager
     {
     }
 }
--- RecipeApps/RecipeSystem/Recipe.cs
+++ RecipeApps/RecipeSystem/Recipe.cs
@@ -1,3 +1,12 @@
-﻿global using NUnit.Framework;
-global using RecipeSystem;
-global using CPUFramework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeSystem
+{
+    public class Recipe
+    {
+    }
+}
