dfe9aada0dcd1d7aa92419e53fa7519fa1fb69f1
puahisrael <puahisrael1@gmail.com>
2023-12-31 12:58:28 -0600

    huh

---
+++ RecipeApps/RecipeSystem/DBManager.cs
@@ -0,0 +1,13 @@
+﻿using CPUFramework;
+
+namespace RecipeSystem
+{
+    public class DBManager
+    {
+        public static void SetConnectionString(string connectionstring)
+        {
+            SQLUtility.ConnectionString = connectionstring;
+        }
+    }
+}
+   
