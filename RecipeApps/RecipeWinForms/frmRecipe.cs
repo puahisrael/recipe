d4042d5b44cd04a0279b0518fe5cef4715778269
puahisrael <puahisrael1@gmail.com>
2023-12-31 12:56:12 -0600

    #42

--- RecipeApps/RecipeWinForms/frmRecipe.cs
+++ RecipeApps/RecipeWinForms/frmRecipe.cs
@@ -1,21 +1,12 @@
 ﻿using CPUFramework;
-using CPUWindowsFormFramework;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace RecipeWinForms
 {
     public partial class frmRecipe : Form 
     {
-        DataTable dtRecipe;
+        DataTable dtrecipe;
 
         public frmRecipe()
         {
@@ -25,52 +16,30 @@ namespace RecipeWinForms
         }
 
 
-        public void ShowForm(int RecipeId)
+        public void ShowForm(int recipeid)
         {
-            string sql = "select r.*, 'Name' = s.FirstName + s.LastName, c.CuisineType from recipe r join staff s on s.staffid = r.staffid join cuisine c on c.cuisineid = r.cuisineid where r.recipeid = " + RecipeId.ToString();
-            dtRecipe = SQLUtility.GetDataTable(sql);
-            DataTable dtStaff = SQLUtility.GetDataTable("select StaffId, LastName from Staff s");
-            DataTable dtCuisine = SQLUtility.GetDataTable("select CuisineId, CuisineType from Cuisine");
-            WindowsFormsUtility.SetListBinding(lstLastName, dtStaff, dtRecipe, "Staff");
-            WindowsFormsUtility.SetListBinding(lstCuisineType, dtCuisine, dtRecipe, "Cuisine");
-            WindowsFormsUtility.SetControlBinding(txtRecipeName, dtRecipe);
-            WindowsFormsUtility.SetControlBinding(txtDraftDate, dtRecipe);
-            WindowsFormsUtility.SetControlBinding(txtPublishedDate, dtRecipe);
-            WindowsFormsUtility.SetControlBinding(txtArchivedDate, dtRecipe);
-            WindowsFormsUtility.SetControlBinding(txtCalories, dtRecipe);
+            dtrecipe = Recipe.Load(recipeid);
+
+            DataTable dtStaff = Recipe.GetStaffList();
+            DataTable dtCuisine = Recipe.GetCuisineList();
+            WindowsFormsUtility.SetListBinding(lstLastName, dtStaff, dtrecipe, "Staff"); 
+            WindowsFormsUtility.SetListBinding(lstCuisineType, dtCuisine, dtrecipe, "Cuisine");
+            WindowsFormsUtility.SetControlBinding(txtRecipeName, dtrecipe);
+            WindowsFormsUtility.SetControlBinding(txtDraftDate, dtrecipe);
+            WindowsFormsUtility.SetControlBinding(txtPublishedDate, dtrecipe);
+            WindowsFormsUtility.SetControlBinding(txtArchivedDate, dtrecipe);
+            WindowsFormsUtility.SetControlBinding(txtCalories, dtrecipe);
             this.Show();
         }
 
         private void Save()
         {
-            SQLUtility.DebugPrintDataTable(dtRecipe);
-            DataRow r = dtRecipe.Rows[0];
-            int id = (int)r["RecipeId"];
-            string sql = "";
-            if(id > 0)
-            {
-                sql = string.Join(Environment.NewLine,
-                    $"update recipe set",
-                    $"StaffId = ' {r["StaffId"]}',",
-                    $"CuisineId = ' {r["CuisineId"]}',",
-                    $"RecipeName = '{r["RecipeName"]}',",
-                    $"DraftDate = '{r["DraftDate"]}',",
-                    $"PublishedDate = '{r["PublishedDate"]}',",
-                    $"ArchivedDate = '{r["ArchivedDate"]}',",
-                    $"Calories = '{r["Calories"]}'",
-                    $"where RecipeId = {r["RecipeId"]}");
-            }
-
-            Debug.Print("-------------");
-
-            SQLUtility.ExecuteSQL(sql);
+            Recipe.Save(dtrecipe);
         }
 
         private void Delete()
         {
-            int id = (int)dtRecipe.Rows[0]["RecipeId"];
-            string sql = "delete recipe where recipeid = " + id;
-            SQLUtility.ExecuteSQL(sql);
+            Recipe.Delete(dtrecipe);
             this.Close();
         }
 
